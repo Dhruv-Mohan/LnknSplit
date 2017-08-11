cca1f417d9c513277ffef69cad061b74ec23d04c
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-08-12 04:33:32 +0530

    Added a comment on spliter and merger objects

--- LnknSplit/LnknSplit/LnknSplit.py
+++ LnknSplit/LnknSplit/LnknSplit.py
@@ -9,6 +9,7 @@ from Utils.Merger import Merger
 split_object = Splitter(file_path='E:\\test.epub', chunk_size=100*1024, output_dir = 'E:\\LnknSplit')
 split_object.split_file()
 
+#Spliter objects create a inputfilename_Header.head file. Merger objects use this file to reconstruct the file split. 
 merge_object = Merger(header_file_path='E:\\LnknSplit\\test_Header.head', output_dir='E:\\LnknSplit\\Testout')
 merge_object.merge_file()
 
